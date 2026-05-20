f50b063feb908976a8f675c5e2d7f98a148d2146
elabbas0 <36539266+elabbas0@users.noreply.github.com>
2026-05-20 21:11:44 +0400

    Initial Spring Init

---
+++ springlms/src/main/java/com/springlms/backend/SpringlmsApplication.java
@@ -0,0 +1,13 @@
+package com.springlms.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringlmsApplication.class, args);
+    }
+
+}
